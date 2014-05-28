1c20d9cbf4743189031ef10ce2ac1039136f3515
andreadelrio <delrio.andre@gmail.com>
2014-05-27 21:28:29 -0500

    q3 working and ready for deployment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,14 +8,38 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.get_ratings
-    @chosen_ratings = @all_ratings
-    @chosen_ratings = params["ratings"].keys if params["ratings"]
-    if params[:sort] == 'title'
-      @movies = Movie.where(:rating => @chosen_ratings).order(:title)
-    elsif params[:sort] == 'release_date'
-      @movies = Movie.where(:rating => @chosen_ratings).order(:release_date)
-    else  
-      @movies = Movie.where(:rating => @chosen_ratings)
+
+    @checked_ratings = params[:ratings] if params.has_key? 'ratings'
+    @ordered_by = params[:order_by] if params.has_key? 'order_by'
+
+    # session.clear where user submit empty rating filters
+    if params.has_key? 'utf8'
+      session.delete :checked_ratings
+      session.delete :ordered_by
+    end
+
+    # update session from incoming params
+    session[:checked_ratings] = @checked_ratings if @checked_ratings
+    session[:ordered_by] = @ordered_by if @ordered_by
+
+    if !@checked_ratings && !@ordered_by && session[:checked_ratings]
+      @checked_ratings = session[:checked_ratings] unless @checked_ratings
+      @ordered_by = session[:ordered_by] unless @ordered_by
+
+      flash.keep
+      redirect_to movies_path({order_by: @ordered_by, ratings: @checked_ratings})
+    end
+
+    if @checked_ratings
+      if @ordered_by      
+        @movies = Movie.find_all_by_rating(@checked_ratings, :order => "#{@ordered_by} asc")
+      else
+        @movies = Movie.find_all_by_rating(@checked_ratings)
+      end
+    elsif @ordered_by
+      @movies = Movie.all(:order => "#{@ordered_by} asc")
+    else 
+      @movies = Movie.all
     end
   end
 
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,16 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def was_highlighted?(column)
+  	@ordered_by==column ? 'hilite' : ''
+  end
+
+  def was_checked?(rating)
+  	@checked_ratings.include? rating unless !@checked_ratings
+  end
+
+  def create_order_params(column)
+  	{order_by: column, ratings: @checked_ratings}
+  end
 end
